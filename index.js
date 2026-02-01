8dd48a59fab3c10b886e864bcbb65e804268aaa8
PentagonCoder <harshu33908@gmail.com>
2026-02-01 11:49:26 +0530

    express server ready

---
+++ index.js
@@ -0,0 +1,25 @@
+
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = process.env.PORT
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/health', (req, res) => {
+  res.send('OK')
+})
+
+app.get('/harsh',(req, res)=>{
+  res.send('<h1>Hello Harsh</h1>')
+})
+
+app.get ('/login',(req, res)=>{
+  res.send('<h1>Login Page</h1>')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
